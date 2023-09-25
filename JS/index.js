48aaffd2fbc5d23bad6b045a5e7bdf7e3095d384
jpalmes01 <jolcurie@gmail.com>
2023-09-25 00:20:29 -0500

    create footer, remove animation on bounce.

--- JS/index.js
+++ JS/index.js
@@ -14,11 +14,66 @@ buttons.forEach(function(button) {
 
   button.addEventListener('mouseout', function() {
     button.style.background = '#DE7875';
-    button.style.color = '#F6CBC2';
+    button.style.color = '#571A0D';
     button.style.fontWeight = 'bold';
     button.style.width = '170px';
     button.style.height = '60px';
   });
 });
 
+$('.hero-button button').click(function() {
+  $('html, body').animate({
+    scrollTop: $('div.footer').offset().top
+  }, 1000);
+});
 
+$('li.inline-block:contains("Contact")').click(function() {
+  $('html, body').animate({
+    scrollTop: $('div.footer').offset().top 
+  }, 1000);
+});
+
+$('.bounceMe').click(function() {
+  $('html, body').animate({
+    scrollTop: $('.grid-item1').offset().top - $('.bounceMe').outerHeight()
+  }, 1000);
+});
+
+$(window).scroll(function() {
+  var top_of_element = $('.grid-item1').offset().top;
+  var bottom_of_element = $('.grid-item1').offset().top + $('.grid-item1').outerHeight();
+  var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+  var top_of_screen = $(window).scrollTop();
+
+  if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+    $('.grid-item1').addClass('in-view');
+  } else {
+    $('.grid-item1').removeClass('in-view');
+  }
+});
+
+$(window).scroll(function() {
+  var top_of_element = $('.grid-item2').offset().top;
+  var bottom_of_element = $('.grid-item2').offset().top + $('.grid-item2').outerHeight();
+  var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+  var top_of_screen = $(window).scrollTop();
+
+  if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+    $('.grid-item2').addClass('in-view');
+  } else {
+    $('.grid-item2').removeClass('in-view');
+  }
+});
+
+$(window).scroll(function() {
+  var top_of_element = $('.grid-item3').offset().top;
+  var bottom_of_element = $('.grid-item3').offset().top + $('.grid-item3').outerHeight();
+  var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+  var top_of_screen = $(window).scrollTop();
+
+  if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+    $('.grid-item3').addClass('in-view');
+  } else {
+    $('.grid-item3').removeClass('in-view');
+  }
+});
